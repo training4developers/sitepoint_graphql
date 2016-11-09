2df222b4bd14c4f99958f252c4e8eef61babe960
Craig Bilner <craig.bilner@gmail.com>
2016-11-09 23:10:00 +0000

    Make suggestions

--- src/graphql/resources.js
+++ src/graphql/resources.js
@@ -15,14 +15,14 @@ export const insertResource = (baseUrl, resourceName, resource) =>
 		body: JSON.stringify(resource)
 	}).then(res => res.json());
 
-export const updateResource = (baseUrl, resourceName, resource) =>
-	fetch(`${baseUrl}/${resourceName}/${resource.id}`, {
-		method: 'put',
-		headers: { 'content-type': 'application/json'},
-		body: JSON.stringify(resource)
-	}).then(res => res.json());
-
-export const deleteResource = (baseUrl, resourceName, resource) =>
-	fetch(`${baseUrl}/${resourceName}/${resource.id}`, {
-		method: 'delete'
-	}).then(res => res.json());
+// export const updateResource = (baseUrl, resourceName, resource) =>
+// 	fetch(`${baseUrl}/${resourceName}/${resource.id}`, {
+// 		method: 'put',
+// 		headers: { 'content-type': 'application/json'},
+// 		body: JSON.stringify(resource)
+// 	}).then(res => res.json());
+//
+// export const deleteResource = (baseUrl, resourceName, resource) =>
+// 	fetch(`${baseUrl}/${resourceName}/${resource.id}`, {
+// 		method: 'delete'
+// 	}).then(res => res.json());
